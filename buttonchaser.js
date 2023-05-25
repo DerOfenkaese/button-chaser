d7559dea8ec63edde7c9532e4fd88973c8ce06b6
DerOfenkaese <jannikschulze@gmail.com>
2023-05-25 16:41:23 +0200

    button movement prototype

--- buttonchaser.js
+++ buttonchaser.js
@@ -2,6 +2,9 @@ function win() {
     document.getElementById("whitespace").innerHTML = "&#9760";
 }
 
-function buttonRun() {
-    document.getElementById("button").style += ""
+function buttonRunVertical() {
+    let ogpos = document.getElementById("button").style.top
+    pos = Number(ogpos.replace("px", ""))
+    console.log(pos)
+    document.getElementById("button").style.top = pos-50+"px";
 }
