7df30ab108c8ba03847c33c90e7da40dbca19dc8
DerOfenkäse <jannikdrost@Janniks-MacBook-Air.local>
2023-05-27 20:21:58 +0200

    less janky button movement (still weird lol)

--- buttonchaser.js
+++ buttonchaser.js
@@ -2,24 +2,34 @@ function win() {
     document.getElementById("whitespace").innerHTML = "&#9760";
 }
 
-function buttonRunVertical() {
+function buttonRunVertical(operator) {
     let ogPos = document.getElementById("button").style.top
     let Pos = Number(ogPos.replace("px", ""))
-    document.getElementById("button").style.top = Pos-50+"px";
+    if (operator=="add") {
+        document.getElementById("button").style.top = Pos+30+"px";
+    }
+    else {
+        document.getElementById("button").style.top = Pos-30+"px";
+    }
 }
 
-function buttonRunHorizontal() {
+function buttonRunHorizontal(operator) {
     let ogPos = document.getElementById("button").style.left
     let Pos = Number(ogPos.replace("px", ""))
-    document.getElementById("button").style.left = Pos-50+"px";
+    if (operator=="add") {
+        document.getElementById("button").style.left = Pos+30+"px";
+    }
+    else {
+        document.getElementById("button").style.left = Pos-30+"px";
+    }
 }
 
 function mousePosRelToButtonPos() {
     let horOgPos = document.getElementById("button").style.left
-    let horPos = Number(horOgPos.replace("px", "")) + 120
+    let horPos = Number(horOgPos.replace("px", "")) + 73
 
     let vertOgPos = document.getElementById("button").style.top
-    let vertPos = Number(vertOgPos.replace("px", ""))
+    let vertPos = Number(vertOgPos.replace("px", "")) + 21.5
 
     window.addEventListener("mousemove", (event) => {
         mousePos = { x: event.clientX, y: event.clientY };
@@ -43,11 +53,19 @@ function mousePosRelToButtonPos() {
     else {
         horDifference = horPos - mousePos.x;
     }
+    //actual movement
 
-    // if (vertDifference < 50 && horDifference < 100) {
-    //     buttonRunVertical();
-    // }
-    // if (horDifference < 50 && vertDifference < 100) {
-    //     buttonRunHorizontal();
-    // }
+    if (mousePos.y > vertPos && vertDifference < 30 && horDifference < 70) {
+        buttonRunVertical("subtract");
+    }
+    if (mousePos.y < vertPos && vertDifference < 30 && horDifference < 70) {
+        buttonRunVertical("add");
+    }
+
+    if (mousePos.x > horPos && horDifference < 85 && vertDifference < 50) {
+        buttonRunHorizontal("subtract");
+    }
+    if (mousePos.x < horPos && horDifference < 85 && vertDifference < 50) {
+        buttonRunHorizontal("add");
+    }
 } 
